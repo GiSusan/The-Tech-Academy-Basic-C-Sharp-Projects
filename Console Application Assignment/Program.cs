72af7d9aff961761413d8296eb11ab18a81649d0
GiSusan <gisusansilva@gmail.com>
2023-06-08 21:10:31 +0100

    Console Application Assignment

---
+++ Console Application Assignment/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Takes an input from the user
+            Console.WriteLine("Enter a number:");
+            string input = Console.ReadLine();
+
+            // Multiplies the input by 50
+            // Converts the input to a decimal for accurate multiplication
+            decimal result1 = Convert.ToDecimal(input) * 50;
+
+            // Prints the result to the console
+            Console.WriteLine("Result 1: " + result1);
+
+            // Adds 25 to the input
+            // Converts the input to an int for integer addition
+            int result2 = Convert.ToInt32(input) + 25;
+
+            // Prints the result to the console
+            Console.WriteLine("Result 2: " + result2);
+
+            // Divides the input by 12.5
+            // Converts the input to a double for floating-point division
+            double result3 = Convert.ToDouble(input) / 12.5;
+
+            // Prints the result to the console
+            Console.WriteLine("Result 3: " + result3);
+
+            // Checks if the input is greater than 50
+            // Converts the input to an int for comparison
+            bool result4 = Convert.ToInt32(input) > 50;
+
+            // Prints the true/false result to the console
+            Console.WriteLine("Result 4: " + result4);
+
+            // Divides the input by 7 and calculates the remainder
+            // Converts the input to an int for remainder calculation
+            int result5 = Convert.ToInt32(input) % 7;
+
+            // Prints the remainder to the console
+            Console.WriteLine("Result 5: " + result5);
+
+            Console.ReadLine();
+        }
+    }
+}
--- Math and Comparison Operators Assignment/Program.cs
+++ Math and Comparison Operators Assignment/Program.cs
@@ -1,26 +1,50 @@
 ﻿using System;
 
 namespace MathAndComparisonOperators
-
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Anonymous Income Comparison Program");
             Console.WriteLine("Person 1");
             Console.WriteLine("Hourly Rate?");
-            string hourlyRate1 = Console.ReadLine();
+            string hourlyRate1Input = Console.ReadLine();
+            decimal hourlyRate1;
+            if (!decimal.TryParse(hourlyRate1Input, out hourlyRate1))
+            {
+                Console.WriteLine("Invalid input for hourly rate of Person 1.");
+                return;
+            }
             Console.WriteLine("Hours worked per week?");
-            string hours1 = Console.ReadLine();
-            int salary1 = Convert.ToInt32(hourlyRate1) * Convert.ToInt32(hours1) * 52;
+            string hours1Input = Console.ReadLine();
+            int hours1;
+            if (!int.TryParse(hours1Input, out hours1))
+            {
+                Console.WriteLine("Invalid input for hours worked per week of Person 1.");
+                return;
+            }
+            decimal salary1 = hourlyRate1 * hours1 * 52;
+
             Console.WriteLine("Person 2");
             Console.WriteLine("Hourly Rate?");
-            string hourlyRate2 = Console.ReadLine();
+            string hourlyRate2Input = Console.ReadLine();
+            decimal hourlyRate2;
+            if (!decimal.TryParse(hourlyRate2Input, out hourlyRate2))
+            {
+                Console.WriteLine("Invalid input for hourly rate of Person 2.");
+                return;
+            }
             Console.WriteLine("Hours worked per week?");
-            string hours2 = Console.ReadLine();
-            int salary2 = Convert.ToInt32(hourlyRate2) * Convert.ToInt32(hours2) * 52;
+            string hours2Input = Console.ReadLine();
+            int hours2;
+            if (!int.TryParse(hours2Input, out hours2))
+            {
+                Console.WriteLine("Invalid input for hours worked per week of Person 2.");
+                return;
+            }
+            decimal salary2 = hourlyRate2 * hours2 * 52;
+
             Console.WriteLine("Annual salary of Person 1:");
             Console.WriteLine(salary1);
             Console.WriteLine("Annual salary of Person 2:");
@@ -32,8 +56,6 @@ namespace MathAndComparisonOperators
             Console.WriteLine(isMore);
 
             Console.ReadLine();
-
-
         }
     }
 }
