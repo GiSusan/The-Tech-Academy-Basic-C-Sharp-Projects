02a1b85c3dd0468ad7af3b7367a4131903275375
GiSusan <gisusansilva@gmail.com>
2023-06-17 10:44:11 +0100

    Add Calling Methods Assignment

---
+++ Calling Methods Assignment/MathOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MathOperationsApp
+{
+    // Class containing math operations
+    public class MathOperations
+    {
+        // Method to double the input number
+        public int DoubleNumber(int number)
+        {
+            return number * 2;
+        }
+
+        // Method to square the input number
+        public int SquareNumber(int number)
+        {
+            return number * number;
+        }
+
+        // Method to calculate the factorial of the input number
+        public int Factorial(int number)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
---
+++ Calling Methods Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MathOperationsApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Prompt user for a number
+            Console.Write("Enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // Create an instance of MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Call each method and display the result
+            int doubledNumber = mathOps.DoubleNumber(userInput);
+            Console.WriteLine("Double: " + doubledNumber);
+
+            int squaredNumber = mathOps.SquareNumber(userInput);
+            Console.WriteLine("Square: " + squaredNumber);
+
+            int factorial = mathOps.Factorial(userInput);
+            Console.WriteLine("Factorial: " + factorial);
+
+            // Wait for user input before exiting
+            Console.ReadLine();
+        }
+    }
+}
