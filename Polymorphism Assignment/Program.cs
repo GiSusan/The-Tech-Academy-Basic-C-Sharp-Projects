1375d7af704785520dd3c78e10679162941d7346
GiSusan <gisusansilva@gmail.com>
2023-06-18 15:29:36 +0100

    Add Polymorphism Assignment Submission

---
+++ Polymorphism Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    // Step 1: Create an interface called IQuittable with a Quit() method
+    public interface IQuittable
+    {
+        void Quit();
+    }
+
+    // Step 2: Employee class inherits from IQuittable interface and implements Quit() method
+    public class Employee : IQuittable
+    {
+        public string Name { get; set; }
+
+        // Implementing the Quit() method form the IQuittable interface
+        public void Quit()
+        {
+            Console.WriteLine("Employee quits the job."); // Display a message when the Quit() method is called
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Step 3: Using polymorphism to create an object of type IQuittable and call Quit() method
+            IQuittable quittableEmployee = new Employee();
+            quittableEmployee.Quit();
+
+            // Output: "Employee quits the job."
+        }
+    }
+}
---
+++ Polymorphism Assignment/README.md
@@ -0,0 +1,22 @@
+# Polymorphism Assignment Submission
+
+This is a console application that demonstrates the usage of interfaces and polymorphism in C#.
+
+## Description
+
+The application includes the following features:
+
+1. An interface called `IQuittable` is defined with a method called `Quit()`.
+2. The `Employee` class inherits from the `IQuittable` interface and implements the `Quit()` method.
+3. Polymorphism is used to create an object of type `IQuittable` and call the `Quit()` method on it.
+
+## Usage
+
+To run the application:
+
+1. Clone or download the project to your local machine.
+2. Open the project in Visual Studio.
+3. Build the solution to ensure there are no errors.
+4. Press the F5 key or click the Start button to run the console application.
+
+You should see the output message "Employee quits the job." displayed in the console window.
